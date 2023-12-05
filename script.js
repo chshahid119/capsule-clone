14b116060e242dd72a579ec017feadf6395a954e
Shahid Chaudhary <133282511+chshahid119@users.noreply.github.com>
2023-12-06 03:54:45 +0500

    fix some problems

---
+++ script.js
@@ -0,0 +1,5 @@
+// const features = document.querySelector(".features");
+// menu_options = document.querySelector(".menu-options-section");
+// features.addEventListener("click", function () {
+//   print(menu_options.classList);
+// });
