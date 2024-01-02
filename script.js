48141af55f18f12baa13188bc2cbc600b4d16eb0
chshahid119 <shahidchaudhary0729@gmail.com>
2024-01-03 01:28:45 +0500

    more changes

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
 // Implementations for hamburger working 
 hamburgerBtn=document.querySelector(".responsive-hamburger")
 mobileMenu=document.querySelector(".hamburger-navbar")
+// section_header=document.querySelector(".section-header")
+hero_section=document.querySelector(".hero-section")
+section_header=document.querySelector('.section-header')
 
 hamburgerBtn.addEventListener("click",()=>{
     console.log("helloWorld")
-    mobileMenu.classList.toggle("displayNone")
-   
-    
+    mobileMenu.classList.toggle("displayNone")   
 })
+
+
+// Sticky Navigation Bar Implemetation 
+
+
+// We cannot use that method to make our navbar sticky because it changes due to viewport according to device 
+window.addEventListener('scroll',function(){
+    console.log(window.scrollY)
+    if(window.scrollY > 20){
+        section_header.style.position="sticky"
+    }
+})
+
+
+
+
+// Smooth Scrolling for all types of navigations 
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroling back to top when click on logo of site
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to other links when click on any btn or nav menu
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // Close mobile navigation after click on nav link
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
 
 
 // Fixing flexbox gap property missing in some Safari Version
